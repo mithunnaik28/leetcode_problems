6c93a5a548a3e9e761e5b9b0c61c1f851c9a02ca
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-09-01 22:34:57 +0530

    Create add digits.py

---
+++ python/add digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        num=num
+        while num>=10:
+            total=0
+            for digit in str(num):
+                total += int(digit)
+            num=total
+        return num
